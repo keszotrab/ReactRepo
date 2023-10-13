6b7796c999caee616286c4b2a7320e8a4fdbc291
unknown <Bartosz.Adamek@microsoft.wsei.edu.pl>
2023-10-13 11:09:24 +0200

    2nd commit

---
+++ my-app/src/components/routes/App.tsx
@@ -0,0 +1,14 @@
+import {FC} from 'react';
+import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+// https://jsonplaceholder.typicode.com/
+// https://education.github.com/git-cheat-sheet-education.pdf
+
+export const App: FC = () => {
+  return (
+    <>App</> 
+  );
+}
+
+//export default App;
---
+++ my-app/src/components/routes/Home.tsx
@@ -0,0 +1,10 @@
+import {FC} from 'react';
+import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+
+export const Home: FC = () => {
+  return (
+    <>Home</>
+  );
+}
